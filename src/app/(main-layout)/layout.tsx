bd518a5df738b5fd1fd0493fa30f3cba7cab01ad
Hilola22 <hilolaorazaliyeva2203@gmail.com>
2025-10-14 13:53:48 +0500

    first commit

---
+++ src/app/(main-layout)/layout.tsx
@@ -0,0 +1,8 @@
+import React, { ReactNode } from "react";
+
+const MainLayout = ({ children }: { children: ReactNode }) => {
+  return (<>Hello</>
+  );
+};
+
+export default MainLayout;
---
+++ src/app/(main-layout)/page.tsx
@@ -0,0 +1,16 @@
+import { memo } from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+export default memo(Home);
+
+// SSR - Server Side Rendering
+// CSR - Client Side Rendering
+// SSG - Static Site Generation
+// ISR - Incremental Static Regeneration
