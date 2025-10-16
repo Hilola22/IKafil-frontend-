12e7fac7fc17a6c199bd22acc7b37d3ad85ca14f
Hilola22 <hilolaorazaliyeva2203@gmail.com>
2025-10-16 14:23:49 +0500

    device view

--- src/app/(main-layout)/page.tsx
+++ src/app/(main-layout)/page.tsx
@@ -1,15 +1,25 @@
+import { DeviceView } from "../../components/device-view/DeviceView";
 import BackTo from "../../components/header/BackTo";
 import Hero from "../../components/hero/Hero";
 
-export default function Home() {
+const Home = async() =>{
 
+  const response = await fetch(
+    "http://3.76.183.255:3030/api/devices?page=1&limit=10",
+    {
+      cache: "force-cache",
+    }
+  );
+  const data = await response.json();
     return (
       <main className="bg-white text-gray-900">
         <Hero />
+        <DeviceView data={data.data} />
         <BackTo />
       </main>
     );
 }
 
+export default Home
 
 
