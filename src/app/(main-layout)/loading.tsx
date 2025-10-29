53a819ad8b9b50dd6065d486501ddbf43a241453
Hilola22 <hilolaorazaliyeva2203@gmail.com>
2025-10-29 11:46:09 +0500

    loading files

---
+++ src/app/(main-layout)/loading.tsx
@@ -0,0 +1,17 @@
+"use client"; 
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-full bg-white">
+      <DotLottieReact
+        src="https://lottie.host/23be50a5-0ed2-4c71-91c4-8865b656cb91/Qnkhz7Yf5Q.lottie"
+        loop
+        autoplay
+        style={{ width: 100, height: 100 }}
+      />
+    </div>
+  );
+};
+
+export default Loader;
