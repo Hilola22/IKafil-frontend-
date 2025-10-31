03d34f3da02d957002ad626e70391cd8d1c1d850
elbekdamirov <elbekmain@gmail.com>
2025-10-31 14:44:20 +0500

    loading detail

--- src/app/(main-layout)/products/[id]/loading.tsx
+++ src/app/(main-layout)/products/[id]/loading.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+"use client";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
-const Loading = () => {
+const Loader = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <div className="flex justify-center items-center h-screen w-full bg-white">
+      <DotLottieReact
+        src="https://lottie.host/23be50a5-0ed2-4c71-91c4-8865b656cb91/Qnkhz7Yf5Q.lottie"
+        loop
+        autoplay
+        style={{ width: 50, height: 50 }}
+      />
+    </div>
+  );
+};
 
-export default Loading
+export default Loader;
