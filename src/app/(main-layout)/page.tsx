fc7c17807ceec0ba970d9be138d02c099eebcae4
Hilola22 <hilolaorazaliyeva2203@gmail.com>
2025-10-31 16:23:42 +0500

    reset password

--- src/app/(main-layout)/page.tsx
+++ src/app/(main-layout)/page.tsx
@@ -31,10 +31,12 @@ const Home = async () => {
       <CategoryView />
       <div>
         <div className="container mx-auto flex justify-between items-center py-10  px-4">
-          <h2 className="text-3xl font-bold">Top products</h2>
+          <h2 className="lg:text-3xl md:text-xl text-[20px] font-bold">
+            Top products
+          </h2>
           <Link
             href={"/products"}
-            className="hover:text-indigo-500 relative inline-block group transition-all duration-300"
+            className="hover:text-indigo-500 relative inline-block group transition-all lg:text-xl md:text-[18px] text-[13px] duration-300"
           >
             View all products
             <span className="absolute left-0 -bottom-0.5 w-0 h-[1px] bg-indigo-500 transition-all duration-300 group-hover:w-full"></span>
